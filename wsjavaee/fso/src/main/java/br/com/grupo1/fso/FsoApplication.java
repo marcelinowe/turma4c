ad0baff66866095f8a71f0f1838792ac7624733d
Marcel Inowe <marcel.inowe@gmail.com>
2020-07-02 10:28:12 -0300

    commit inicial do projeto final do grupo1 da turma4c

---
+++ wsjavaee/fso/src/main/java/br/com/grupo1/fso/FsoApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupo1.fso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsoApplication.class, args);
+	}
+
+}
---
+++ wsjavaee/fso/src/test/java/br/com/grupo1/fso/FsoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grupo1.fso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
