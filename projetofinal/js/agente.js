0558e73395d621b5bec4b6f09d215ff8b511999e
Marcel Inowe <marcel.inowe@gmail.com>
2020-07-03 10:06:30 -0300

    inclusao do droplist de agente financeiro

--- projetofinal/js/agente.js
+++ projetofinal/js/agente.js
@@ -1,4 +1,4 @@
-function exibirUsuario(){
+function carregaAgente(){
     var usuarioLogado = localStorage.getItem("user");
 
     if(!usuarioLogado){
@@ -11,9 +11,31 @@ function exibirUsuario(){
 
         document.getElementById("fotoUsuario").innerHTML = 
         "<img width = '20%' src=images/"+usuarioJson.foto+">"
+
+        exibirAgentes();
     }
 }
 function logoff(){
     localStorage.removeItem("user");
     window.location = "login.html";
+}
+
+function exibirAgentes(){
+    fetch("http://localhost:8080/agentes").then(res=>res.json()).then(res=>popula(res));
+}
+
+function popula(lista){
+    var dropAgente = 
+    '<div class="input-group mb-3">'+
+    '<div class="input-group-prepend">'+
+      '<label class="input-group-text" for="inputGroupSelect01">Agente</label>'+
+    '</div>'+
+    '<select class="custom-select" id="inputGroupSelect01">'+
+      '<option selected>Escolha o agente financeiro</option>';
+      for(i=0;i<lista.length;i++){
+        dropAgente+='<option value="'+lista[i].id+'">'+lista[i].nomeAgente+'</option>';
+      }
+      dropAgente+='</select></div>';
+
+      document.getElementById("agentes").innerHTML = dropAgente;
 }
