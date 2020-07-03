5a4a31fac17d87e5ad2cc02929d008bb076fe27e
Marcel Inowe <marcel.inowe@gmail.com>
2020-07-03 10:31:13 -0300

    inclusao de evento no droplist de agentes

--- projetofinal/js/agente.js
+++ projetofinal/js/agente.js
@@ -13,6 +13,7 @@ function carregaAgente(){
         "<img width = '20%' src=images/"+usuarioJson.foto+">"
 
         exibirAgentes();
+        exibirAgentesTop10();
     }
 }
 function logoff(){
@@ -21,16 +22,20 @@ function logoff(){
 }
 
 function exibirAgentes(){
-    fetch("http://localhost:8080/agentes").then(res=>res.json()).then(res=>popula(res));
+    fetch("http://localhost:8080/agentes").then(res=>res.json()).then(res=>populaDrop(res));
 }
 
-function popula(lista){
+function exibirAgentesTop10(){
+    fetch("http://localhost:8080/agentestop10").then(res=>res.json()).then(res=>populaTable(res));
+}
+
+function populaDrop(lista){
     var dropAgente = 
     '<div class="input-group mb-3">'+
     '<div class="input-group-prepend">'+
       '<label class="input-group-text" for="inputGroupSelect01">Agente</label>'+
     '</div>'+
-    '<select class="custom-select" id="inputGroupSelect01">'+
+    '<select class="custom-select" id="inputGroupSelect01" onchange="dashAgente()">'+
       '<option selected>Escolha o agente financeiro</option>';
       for(i=0;i<lista.length;i++){
         dropAgente+='<option value="'+lista[i].id+'">'+lista[i].nomeAgente+'</option>';
@@ -38,4 +43,22 @@ function popula(lista){
       dropAgente+='</select></div>';
 
       document.getElementById("agentes").innerHTML = dropAgente;
+}
+
+function populaTable(lista){
+      var listaAgentes=
+      '<table align="center" width="90%" border="1" cellpadding="10" cellspacing="15">'+
+      '<tr>'+
+          '<th>Parceiro</th>'+
+          '<th>Volume Transacional</th>'+
+      '</tr>';
+      for(i=0;i<lista.length;i++){
+        listaAgentes += '<tr align="center"><td >'+lista[i].nomeAgente+'</td><td>'+lista[i].volumeTransacional+'</td></tr>';
+      }
+      listaAgentes +='</table>'
+      document.getElementById("listatop10").innerHTML = listaAgentes;
+}
+
+function dashAgente(){
+    alert("hello....")
 }
