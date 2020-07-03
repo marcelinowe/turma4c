098abc808f71e88da964453071a32a15d4d26ba3
Marcel Inowe <marcel.inowe@gmail.com>
2020-07-03 15:45:50 -0300

    inclusao de comentarios

--- projetofinal/js/funcoes.js
+++ projetofinal/js/funcoes.js
@@ -1,8 +1,14 @@
 function login(){
+    /*
+    Criação da mensagem que será enviada via método POST
+    */
     var mensagem = {
         email: document.getElementById("txtEmail").value,
         senha: document.getElementById("txtSenha").value
     };
+    /*
+    Criação de cabeçalho HTTP para envio do método post para validação de login
+    */
     var cabecalho ={
         method: "POST",
         body: JSON.stringify(mensagem),
@@ -10,7 +16,10 @@ function login(){
             "Content-Type" : "application/json"
         }
     };
-
+    /*
+    Chamada assíncrona da validação de login;
+    Nesse passo é enviado um objeto Usuario;
+    */
     fetch("http://localhost:8080/login", cabecalho)
         .then(res => res.json())
         .then(res =>{
